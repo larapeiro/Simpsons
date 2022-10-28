53721a3605e85491ea0dc6ddf0b77b12cc4a8aff
Lara <larapeiro.a@gmail.com>
2022-10-28 11:45:17 +0200

    Últimas correcciones

--- LisaLevel/main.py
+++ LisaLevel/main.py
@@ -23,10 +23,10 @@ while True:
 #Extraemos los datos del JSON
   URL = "https://thesimpsonsquoteapi.glitch.me/quotes"
   try:
-    respuesta = requests.get(URL,timeout=3)
+    respuesta = requests.get(URL,timeout=30)
   except requests.exceptions.Timeout as error:
     print(error)
-    time.sleep(3)
+    time.sleep(30)
 
   #Añadimos la condición de respuesta. Fuente: https://keepcoding.io/blog/trabajar-con-codigos-estado-de-respuesta-http/#Condicion_de_la_respuesta
   if respuesta.status_code >= 200 and respuesta.status_code < 300:
@@ -44,19 +44,19 @@ while True:
   #Indicamos el diccionario que tiene que actualizarse a través de la función definida anteriormente y sus atributos
   dict_palabras=contador(frase_simpson, dict_palabras)
 #Generamos el documento que contendrá el contador
-  with open('LisaLevel/palabras.csv', 'w') as csvfile:
+  with open('palabras.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
     for key, value in dict_palabras.items():
       writer.writerow([key, value]) 
 
 #Creamos el directorio para cada personaje. Para ello, en primer lugar tenemos que comprobar si ya existe el directorio o no.
 # Fuente: https://www.delftstack.com/es/howto/python/python-create-directory/#crear-directorio-en-python-usando-los-m%25C3%25A9todos-path.exists-y-makedirs-del-m%25C3%25B3dulo-os
-  if not os.path.exists('LisaLevel/'+ "_".join(datos["Personaje"])):
-    os.makedirs('LisaLevel/' + "_".join(datos["Personaje"]))
+  if not os.path.exists("_".join(datos["Personaje"])):
+    os.makedirs("_".join(datos["Personaje"]))
 
 #A continuación, tenemos que almacenar la frase de cada personaje en su correspondiente csv indicando la ruta (LisaLevel)
 
-  with open ('LisaLevel/' + "_".join(datos["Personaje"]) + '/' + 'quotes' + "" + "_".join(datos["Personaje"]) + '.csv', 'a') as directorio:
+  with open ("_".join(datos["Personaje"]) + '/' + 'quotes' + "" + "_".join(datos["Personaje"]) + '.csv', 'a') as directorio:
     w = csv.DictWriter(directorio, datos.keys())
     if directorio.tell() == 0:
       w.writeheader()
@@ -64,9 +64,9 @@ while True:
       w.writerow({'Personaje':" ".join(datos["Personaje"]), 'Frase': datos["Frase"]})
   
 #Descargamos y almacenamos la imagen del personaje en su carpeta correspondiente indicando la ruta (LisaLevel)
-  with open('LisaLevel/' + "_".join(datos["Personaje"]) + '/' + "".join(datos["Personaje"]) +'.png', 'wb') as handler:
+  with open("_".join(datos["Personaje"]) + '/' + "".join(datos["Personaje"]) +'.png', 'wb') as handler:
     if handler.tell() == 0:
       imagen_personaje= requests.get(imagen).content
       handler.write(imagen_personaje)
 
-time.sleep(3)
+time.sleep(30)
--- MaggieLevel/main.py
+++ MaggieLevel/main.py
@@ -13,28 +13,29 @@ while True:
   frase_simpson: str= datos[0]['quote']
   autor: str= datos[0]['character']
   
+  
   #Añadimos las condicionales indicando la ruta donde tiene que almacenarse y la información que almacena el diccionario
   if autor == 'Lisa Simpson':
     my_dict = {'quote': frase_simpson, 'character':autor}
-    with open('MaggieLevel/Lisa/lisa.csv', 'a') as csvfile:
+    with open('Lisa/lisa.csv', 'a') as csvfile:
       w = csv.DictWriter(csvfile, my_dict.keys())
 
       w.writerow(my_dict)
     
   elif autor == 'Homer Simpson':
     my_dict = {'quote': frase_simpson, 'character':autor}
-    with open('MaggieLevel/Homer/homer.csv', 'a') as csvfile:
+    with open('Homer/homer.csv', 'a') as csvfile:
       w = csv.DictWriter(csvfile, my_dict.keys())
 
       w.writerow(my_dict)
 
-  else:
+  
       
-    my_dict = {'quote': frase_simpson, 'character':autor}
-    with open('MaggieLevel/General/general.csv', 'a') as csvfile:
-      w = csv.DictWriter(csvfile, my_dict.keys())
+  my_dict = {'quote': frase_simpson, 'character':autor}
+  with open('General/general.csv', 'a') as csvfile:
+    w = csv.DictWriter(csvfile, my_dict.keys())
 
-      w.writerow(my_dict)
+    w.writerow(my_dict)
 
   #Tiempo que indicamos para que vuelva a correr el código
   time.sleep(30)
